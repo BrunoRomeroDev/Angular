fc8967da45b783043a0fb6b64cb0ab587aadc8da
Bruno Romero <romero_bruno@msn.com>
2023-02-06 23:08:02 -0300

    connection sping boot

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,8 +1,11 @@
-import { CoursesService } from './../services/courses.service';
-import { Component } from '@angular/core';
-import { Course } from '../model/course';
-import { Observable } from 'rxjs/internal/Observable';
+import { ErroDialogComponent } from './../../shared/components/erro-dialog/erro-dialog.component';
+import { Component,OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
+import { Course } from '../model/course';
+import { CoursesService } from './../services/courses.service';
+import {catchError} from 'rxjs/operators';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-courses',
@@ -14,9 +17,22 @@ export class CoursesComponent  {
   courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor(private CoursesService: CoursesService){
+  constructor(private CoursesService: CoursesService,
+    public dialog: MatDialog){
 
-    this.courses$ = this.CoursesService.list();
+    this.courses$ = this.CoursesService.list()
+    .pipe(
+      catchError(erro => {
+        this.onError('Erro ao carregar.');
+        return of([])
+      })
+    );
+    }
+
+    onError(errorMsg: string) {
+      this.dialog.open(ErroDialogComponent, {
+        data: errorMsg
+      });
     }
 
   ngOnInit(): void{
